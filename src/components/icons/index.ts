3d10350386edf65cf40120951c75295c35a1910d
007qr <aayushpatil558321@gmail.com>
2024-05-08 23:09:15 +0530

    added mobile navigation

---
+++ src/components/icons/index.ts
@@ -0,0 +1,6 @@
+import {Settings} from "@/components/icons/settings";
+import {Calendar} from "@/components/icons/calendar";
+import {Patients} from "@/components/icons/patients";
+import {Notification} from "@/components/icons/notification";
+
+export {Settings, Calendar, Patients, Notification}
